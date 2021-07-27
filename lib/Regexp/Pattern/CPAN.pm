f411917c3a1dc444aee1879b3550b609ee333904
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-27 13:58:44 +0700

    [refactor] Move release_tarball_filename pattern to Regexp-Pattern-Perl dist under Regexp::Pattern::Perl::Release

--- lib/Regexp/Pattern/CPAN.pm
+++ lib/Regexp/Pattern/CPAN.pm
@@ -23,21 +23,6 @@ _
             {str=>'RANDALSCHWARTZ', gen_args=>{-anchor=>1}, summary=>'too long', matches=>0},
         ],
     },
-
-    release_tarball_filename => {
-        summary => 'Proper filename of a typical distribution release tarball',
-        pat => qr/([A-Za-z_][A-Za-z0-9_]*(?:-[A-Za-z0-9_][A-Za-z0-9_]*)*)-
-                  v?([0-9]+(?:\.[0-9]+){0,4}(?:_[0-9]+|-TRIAL)?)
-                  \.(tar|tar\.(?:Z|gz|bz2|xz)|zip|rar)/x,
-        tags => ['capturing'],
-        examples => [
-            {str=>'Acme-CPANModulesBundle-Import-PerlAdvent-2000-0.001.tar.gz', gen_args=>{-anchor=>1}, matches=>1},
-            {str=>'Acme-CPANModulesBundle-Import-PerlAdvent-2000-v0.001.tar.gz', gen_args=>{-anchor=>1}, matches=>1, summary=>'v prefix before version number allowed'},
-            {str=>'0.001.tar.gz', gen_args=>{-anchor=>1}, matches=>0, summary=>'No distribution name'},
-            {str=>'Acme-CPANModulesBundle-Import-PerlAdvent-2000.tar.gz', gen_args=>{-anchor=>1}, matches=>1, summary=>'Unfortunately, numeric namespace name gets mistaken as version number'},
-            {str=>'Acme-CPANModulesBundle-Import-PerlAdvent.tar.gz', gen_args=>{-anchor=>1}, matches=>0, summary=>'No version number'},
-        ],
-    },
 );
 
 1;
